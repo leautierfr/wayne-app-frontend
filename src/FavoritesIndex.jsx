0319de460af43e601f68ff5d309710b1228424af
Francois-Rene <leautier.fr@gmail.com>
2023-01-09 12:52:34 -0500

    cleans up FavoritesIndex

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -1,13 +1,9 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-
 export function FavoritesIndex(props) {
   return (
     <div>
       <h1>All favorites</h1>
       {props.favorites.map((favorite) => (
         <div key={favorite.id}>
-          <h2>{favorite.user_id}</h2>
           <h2>{favorite.song_id}</h2>
         </div>
       ))}

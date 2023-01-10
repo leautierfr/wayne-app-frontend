c274d546815fbb65b4fb32538c63a83f56282d93
Francois-Rene <leautier.fr@gmail.com>
2023-01-10 16:49:02 -0500

    adds (non-functional) unfavorite button

--- src/FavoritesHome.jsx
+++ src/FavoritesHome.jsx
@@ -3,6 +3,12 @@ import axios from "axios";
 import { FavoritesIndex } from "./FavoritesIndex";
 export function FavoritesHome() {
   const [favorites, setFavorites] = useState([]);
+  const [isFavoritesShowVisible, setIsFavoritesShowVisible] = useState(false);
+  const [currentFavorite, setCurrentFavorite] = useState([]);
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsFavoritesShowVisible(false);
+  };
 
   const handleIndexFavorites = () => {
     console.log("handleIndexFavorites");
@@ -11,13 +17,22 @@ export function FavoritesHome() {
       setFavorites(response.data);
     });
   };
+  console.log(favorites);
+
+  const handleDestroyFavorite = (favorite) => {
+    console.log("handleDestroyFavorite", favorite);
+    axios.delete(`http://localhost:3000/favorites/${favorite.id}.json`).then((response) => {
+      setFavorites(favorites.filter((f) => f.id !== favorite.id));
+      handleClose();
+    });
+  };
 
   useEffect(handleIndexFavorites, []);
   return (
     <div>
       <div class="card">
         <div class="card-body"></div>
-        <FavoritesIndex favorites={favorites} />
+        <FavoritesIndex favorites={favorites} onDestroyFavorite={handleDestroyFavorite} />
       </div>
     </div>
   );
--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
 export function FavoritesIndex(props) {
+  const [favorites, setFavorites] = useState([]);
+  const handleClick = () => {
+    props.onDestroyFavorite(props.favorite);
+  };
+
   return (
     <div>
       <h1>All favorites</h1>
@@ -7,6 +13,7 @@ export function FavoritesIndex(props) {
           <h2>
             {favorite.user_id}/{favorite.song_id}
           </h2>
+          <button onClick={handleClick}>Unfavorite</button>
         </div>
       ))}
     </div>

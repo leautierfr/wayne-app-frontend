faab07f26b9e1fadc280798d2974cd6332e24475
Francois-Rene <leautier.fr@gmail.com>
2023-02-15 20:06:31 -0500

    favorites now display song titles

--- src/FavoritesIndex.jsx
+++ src/FavoritesIndex.jsx
@@ -8,9 +8,7 @@ export function FavoritesIndex(props) {
       <h1>All favorites</h1>
       {props.favorites.map((favorite) => (
         <div key={favorite.id}>
-          <h2>
-            {favorite.user_id}/{favorite.song_id}
-          </h2>
+          <h2>{favorite.song}</h2>
           <button onClick={() => props.onDestroyFavorite(favorite)}>Unfavorite</button>
         </div>
       ))}
